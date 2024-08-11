24f8aac7b33f2365a514be40064ed5867efc2e80
gokubyte1 <anujrithalia301@gmail.com>
2024-08-11 20:46:14 +0530

    Initial commit

---
+++ nodeServer/Server/config/firebase.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { initializeApp } = require("firebase/app");
+const admin = require('firebase-admin');
+const serviceAccount = require("./firebaseService.json");
+
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
+// };
+const firebaseConfig = {
+  //not to be public
+};
+const fireApp = initializeApp(firebaseConfig);
+
+
+console.log(process.env.FIREBASE_API_KEY);
+const adminFireApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+module.exports = {fireApp, adminFireApp};
